7831068b8b5976391d848c5510787ee61e1fa798
ch-zacmo <zacmonnet@gmail.com>
2024-06-20 13:55:28 +0200

    added clamp function and comparison for value

--- src/utils.js
+++ src/utils.js
@@ -13,6 +13,25 @@ function removeLeadingZeros(value) {
 	return value.replace(/^0+/, "");
 }
 
+
+// Function to clamp values
+function clamp(val, min, max) {
+	return Math.max(min, Math.min(max, val));
+}
+
+
+// Function to compare two values
+function compareValues(val1, val2) {
+	if (typeof val1 !== typeof val2) return false;
+	if (typeof val1 === 'object') {
+		return JSON.stringify(val1) === JSON.stringify(val2);
+	}
+	return val1 === val2;
+}
+
+
 module.exports = {
-    removeLeadingZeros,
+	removeLeadingZeros,
+	clamp,
+	compareValues
 };
